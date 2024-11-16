4afc94a80b708e8069779a210a2cdfff722cf68c
kvarcev <0-zh@mail.ru>
2024-11-16 22:08:28 +0400

    fixes

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,52 +1,52 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import { useSelector } from 'react-redux';
-import {useNavigate, Navigate, useParams } from "react-router-dom";
-import axios from '../../axios';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import SimpleMDE from 'react-simplemde-editor';
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import SimpleMDE from "react-simplemde-editor";
+import axios from "../../axios";
 
-import 'easymde/dist/easymde.min.css';
+import "easymde/dist/easymde.min.css";
 import { selectIsAuth } from "../../redux/slices/auth";
 
-import styles from './AddPost.module.scss';
+import styles from "./AddPost.module.scss";
 
 export const AddPost = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
   const isAuth = useSelector(selectIsAuth);
   const [isLoading, setLoading] = React.useState(false);
-  const [text, setText] = React.useState('');
-  const [title, setTitle] = React.useState('');
-  const [tags, setTags] = React.useState('');
-  const [imageUrl, setImageUrl] = React.useState('');
+  const [text, setText] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [tags, setTags] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
   const inputFileRef = React.useRef(null);
 
   const isEditing = Boolean(id);
 
-  const handleChangeFile = async(event) => {
-try {
-  const formData = new FormData();
-  const file = event.target.files[0];
-  formData.append('image', file);
-  const { data } = await axios.post('/upload', formData);
-  setImageUrl(data.url);
-} catch (err) {
-  console.warm(err);
-  alert('Ошибка при загрузке файла');
-}
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("/upload", formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warm(err);
+      alert("Ошибка при загрузке файла");
+    }
   };
 
   const onClickRemoveImage = () => {
-    setImageUrl('');
+    setImageUrl("");
   };
 
   const onChange = React.useCallback((value) => {
     setText(value);
   }, []);
 
-  const onSubmit = async() => {
+  const onSubmit = async () => {
     try {
       setLoading(true);
 
@@ -54,45 +54,45 @@ try {
         title,
         imageUrl,
         tags,
-        text
+        text,
       };
 
-      const {data} = isEditing
-      ? await axios.patch(`/posts/${id}`, fields)
-      : await axios.post(`/posts`, fields);
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, fields)
+        : await axios.post(`/posts`, fields);
 
       const _id = isEditing ? id : data._id;
 
       navigate(`/posts/${_id}`);
     } catch (err) {
       console.warn(err);
-      alert('Ошибка при создании');
-
+      alert("Ошибка при создании");
     }
   };
 
   React.useEffect(() => {
     if (id) {
-      axios.get(`/posts/${id}`)
-      .then(({ data }) =>{
-        setTitle(data.title);
-        setText(data.text);
-        setImageUrl(data.imageUrl);
-        setTags(data.tags.join(','));
-      })
-      .catch(err =>{
-        console.warn(err);
-        alert('Ошибка при получении статьи');
-      })
+      axios
+        .get(`/posts/${id}`)
+        .then(({ data }) => {
+          setTitle(data.title);
+          setText(data.text);
+          setImageUrl(data.imageUrl);
+          setTags(data.tags.join(","));
+        })
+        .catch((err) => {
+          console.warn(err);
+          alert("Ошибка при получении статьи");
+        });
     }
   }, []);
 
   const options = React.useMemo(
     () => ({
       spellChecker: false,
-      maxHeight: '400px',
+      maxHeight: "400px",
       autofocus: true,
-      placeholder: 'Введите описание проекта',
+      placeholder: "Введите описание проекта",
       status: false,
       autosave: {
         enabled: true,
@@ -102,52 +102,79 @@ try {
     [],
   );
 
-  if (!window.localStorage.getItem('token') && !isAuth) {
+  if (!window.localStorage.getItem("token") && !isAuth) {
     return <Navigate to="/" />;
   }
 
-  console.log({title, tags, text})
+  console.log({ title, tags, text });
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
         Загрузить превью
       </Button>
-      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
       {imageUrl && (
         <>
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-        <div className={styles.imageContainer}>
-        <br></br>
-        <div className={styles.divimage} style={{backgroundImage: `url(${`${process.env.REACT_APP_API_URL}${imageUrl}`})`}} alt="Uploaded"></div>
-        {/* <img className={styles.image}  src={`${process.env.REACT_APP_API_URL}${imageUrl}`} alt="Uploaded" /> */}
-        </div>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+          <div className={styles.imageContainer}>
+            <br></br>
+            <div
+              className={styles.divimage}
+              style={{
+                backgroundImage: `url(${`https://api.itroom18.ru/mveu/temp/api${imageUrl}`})`,
+              }}
+              alt="Uploaded"
+            ></div>
+            {/* <img className={styles.image}  src={`${process.env.REACT_APP_API_URL}${imageUrl}`} alt="Uploaded" /> */}
+          </div>
         </>
       )}
-      
+
       <br />
       <br />
       <TextField
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Название проекта"
-        value ={title}
-        onChange={e => setTitle(e.target.value)}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         fullWidth
       />
       <TextField
-      value ={tags}
-      onChange={e => setTags(e.target.value)}
-      classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth 
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        classes={{ root: styles.tags }}
+        variant="standard"
+        placeholder="Тэги"
+        fullWidth
       />
       {/* <p className={styles.desc} >Добавьте описание для вашего проекта</p> */}
-      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
-      
+      <SimpleMDE
+        className={styles.editor}
+        value={text}
+        onChange={onChange}
+        options={options}
+      />
+
       <div className={styles.buttons}>
         <Button onClick={onSubmit} size="large" variant="contained">
-          {isEditing ? 'Сохранить' : 'Опубликовать'}
+          {isEditing ? "Сохранить" : "Опубликовать"}
         </Button>
         <a href="/">
           <Button size="large">Отмена</Button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,75 +1,71 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import axios from '../axios';
-
-import { Post } from '../components/Post';
-import { TagsBlock } from '../components/TagsBlock';
-import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts, fetchTags } from '../redux/slices/posts';
-import styles from './Post.module.scss';
-import { colors } from '@mui/material';
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { fetchPosts, fetchTags } from "../redux/slices/posts";
+import styles from "./Post.module.scss";
 
 export const Home = () => {
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.auth.data);
   const { posts, tags } = useSelector((state) => state.posts);
 
-  const isPostsLoading = posts.status ==='loading';
-  const isTagsLoading = tags.status ==='loading';
+  const isPostsLoading = posts.status === "loading";
+  const isTagsLoading = tags.status === "loading";
 
-
-  React.useEffect(() =>{
+  React.useEffect(() => {
     dispatch(fetchPosts());
     dispatch(fetchTags());
   }, []);
 
-
-
   return (
     <>
-      <Tabs TabIndicatorProps={{style: {backgroundColor: "#312285"}
-  }}style={{ marginBottom: 15}} value={0} aria-label="basic tabs example">
+      <Tabs
+        TabIndicatorProps={{ style: { backgroundColor: "#312285" } }}
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
         <Tab label="Проекты" />
         <Tab label="Видео" />
       </Tabs>
-        <Grid xs={4} item>
-          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
-          
-        </Grid>
-      
+      <Grid xs={4} item>
+        <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+      </Grid>
+
       <Grid container>
         {/* <Grid xs={3} item> */}
         <div className={styles.container}>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => 
-          isPostsLoading ? (
-            <Post key={index} isLoading={true}/>
-          ) : (
-            
-              <Grid className={styles.item} >
-              <Post 
-                id={obj._id}
-                title={obj.title}
-                imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`: ''}
-                user={obj.user}
-                // createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                rateCount={5}
-                tags={obj.tags}
-                
-                isEditable = {userData?._id === obj.user._id}
-              />
-              
-            </Grid>
-          ))}
-          </div>
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Grid className={styles.item}>
+                <Post
+                  id={obj._id}
+                  title={obj.title}
+                  imageUrl={
+                    obj.imageUrl
+                      ? `https://api.itroom18.ru/mveu/temp/api${obj.imageUrl}`
+                      : ""
+                  }
+                  user={obj.user}
+                  // createdAt={obj.createdAt}
+                  viewsCount={obj.viewsCount}
+                  rateCount={5}
+                  tags={obj.tags}
+                  isEditable={userData?._id === obj.user._id}
+                />
+              </Grid>
+            ),
+          )}
+        </div>
         {/* </Grid> */}
-        
       </Grid>
-      
     </>
   );
 };
