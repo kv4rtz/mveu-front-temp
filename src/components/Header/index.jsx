8b319d5f40c999f2f38a07912728ab436564717d
kvarcev <0-zh@mail.ru>
2024-11-17 22:31:39 +0400

    adaptive header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Button from '@mui/material/Button';
-import { useDispatch,useSelector } from 'react-redux';
+import Button from "@mui/material/Button";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-import styles from './Header.module.scss';
-import Container from '@mui/material/Container';
-import { logout, selectIsAuth } from '../../redux/slices/auth';
+import Container from "@mui/material/Container";
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+import styles from "./Header.module.scss";
 
 export const Header = () => {
   const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth);
 
   const onClickLogout = () => {
-    if (window.confirm('Вы уверены, что хотите выйти?')) {
+    if (window.confirm("Вы уверены, что хотите выйти?")) {
       dispatch(logout());
-      window.localStorage.removeItem('token');
+      window.localStorage.removeItem("token");
     }
   };
 
   return (
     <div className={styles.root}>
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" style={{ padding: 0 }}>
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-          <div className={styles.logoPng}></div>
+            <div className={styles.logoPng}></div>
             <div>Студенческое портфолио</div>
           </Link>
           <div className={styles.buttons}>
@@ -32,18 +32,26 @@ export const Header = () => {
                 <Link to="/add-post">
                   <Button className={styles.createBtn}>Загрузить проект</Button>
                 </Link>
-                <Button className="logoutBtn" onClick={onClickLogout} variant="contained" color="error">
+                <Button
+                  className="logoutBtn"
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
                   Выйти
                 </Button>
               </>
             ) : (
               <>
                 <Link to="/login">
-                  <Button className={styles.loginBtn} variant="outlined">Войти</Button>
+                  <Button className={styles.loginBtn} variant="outlined">
+                    Войти
+                  </Button>
                 </Link>
                 <Link to="/register">
-                  <Button className={styles.regBtn} variant="contained">Создать аккаунт</Button>
-                  
+                  <Button className={styles.regBtn} variant="contained">
+                    Создать аккаунт
+                  </Button>
                 </Link>
               </>
             )}
